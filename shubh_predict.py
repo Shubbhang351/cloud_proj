68e6b3ccf808e4e58ed8a02de87bc57586f685b6
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-04-19 10:32:18 +0530

    351

---
+++ shubh_predict.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+model_1 = tf.keras.models.load_model('shubh_model35.h5')
+
+read = lambda imname: np.asarray(Image.open(imname).convert("RGB"))
+
+def predict(imag_name):
+
+    img = [read(imag_name)]
+
+    img_1 = np.array(img, dtype='uint8')
+
+    img_2 = img_1/255
+
+    ans = model_1.predict(img_2)
+
+    a = np.argmax(ans, axis = 1)[0]
+
+    if a == 0:
+        return "begnin"
+    else:
+        return "malignant"
+
+
